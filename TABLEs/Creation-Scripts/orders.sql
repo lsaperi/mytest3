0a1a11f6a551cdd9c05c1ef6b742c90f5540b2d0
lsaperi <lsaperi>
2025-07-02 14:27:03 -0300

    hghgfhfg

--- TABLEs/Creation-Scripts/orders.sql
+++ TABLEs/Creation-Scripts/orders.sql
@@ -3,5 +3,6 @@ CREATE TABLE `orders` (
   `customer_name` varchar(100) DEFAULT NULL,
   `order_date` date DEFAULT NULL,
   PRIMARY KEY (`order_id`),
-  KEY `idx_customer_name` (`customer_name`)
+  KEY `idx_customer_name` (`customer_name`),
+  KEY `idx_customer_name2` (`customer_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
---
+++ TABLEs/Creation-Scripts/products.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `products` (
+  `id` int NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
